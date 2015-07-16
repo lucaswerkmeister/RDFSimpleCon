5e9033fe62b719dd538a22c2d8aba85e4bb3e717
Jasper Koehorst <jasperkoehorst@gmail.com>
2015-07-16 11:26:22 +0200

    made 1.7 version of RDFSimpleCon

--- src/main/java/nl/wur/ssb/RDFSimpleCon/concurrent/ResultHandler.java
+++
@@ -1,8 +0,0 @@
-package nl.wur.ssb.RDFSimpleCon.concurrent;
-
-import nl.wur.ssb.RDFSimpleCon.ResultLine;
-
-public interface ResultHandler
-{
-	public void handleResult(ResultLine item,int count) throws Exception;
-}
--- src/main/java/nl/wur/ssb/RDFSimpleCon/concurrent/Task.java
+++
@@ -1,6 +0,0 @@
-package nl.wur.ssb.RDFSimpleCon.concurrent;
-
-public interface Task
-{
-	public void run(Object ... args) throws Exception;
-}
